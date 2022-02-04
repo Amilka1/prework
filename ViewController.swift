51b49e0b0bf88bb3a9804bbb050da3404d5a2c7d
Amilka1 <87542495+Amilka1@users.noreply.github.com>
2022-02-03 21:16:13 -0500

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  prework
+//
+//  Created by Amilka Diaz on 2/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   
+    @IBOutlet weak var billAmountTextfield: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func calculateTip(_ sender: Any) {
+        let bill = Double(billAmountTextfield.text!) ?? 0
+        
+        let tipPercentages = [0.15, 0.18, 0.2]
+        let tip = bill *
+            tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        
+    tipAmountLabel.text = String(format: "$%.2f", tip)
+    totalLabel.text = String(format: "$%.2f", total)
+        
+    }
+    
+}
+
